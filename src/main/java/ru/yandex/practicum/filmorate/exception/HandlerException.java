865bced4945380fb496160cf3b0a86126f4436b6
allon21 <anthon.chub@yandex.ru>
2024-11-23 23:58:03 +0300

    add

--- src/main/java/ru/yandex/practicum/filmorate/exception/HandlerException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/HandlerException.java
@@ -3,14 +3,14 @@ package ru.yandex.practicum.filmorate.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
-@RestControllerAdvice
+@ControllerAdvice
 public class HandlerException {
 
     @ExceptionHandler(ValidationException.class)
@@ -25,13 +25,6 @@ public class HandlerException {
         Map<String, String> errors = new HashMap<>();
         errors.put("Объект не найден", e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errors);
-    }
-
-    @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<?> validIdException(ValidationException e) {
-        Map<String, String> errors = new HashMap<>();
-        errors.put("Некорректное значение", e.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errors);
     }
 
     @ExceptionHandler(Exception.class)
